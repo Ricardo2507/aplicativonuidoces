b7714121eba46fd5e68553308065dbbc97ae9308
Ricardo Sérgio <rscarvalho@hotmail.com>
2020-02-08 20:13:14 -0300

    Testando a primeira classe de domímio - Categoria

--- src/main/java/com/ricardo/nuidoces/resources/CategoriaResource.java
+++ src/main/java/com/ricardo/nuidoces/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.ricardo.nuidoces.resources;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardo.nuidoces.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Doces Simples");
+		Categoria cat2 = new Categoria(2, "Doces Gourmet");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
