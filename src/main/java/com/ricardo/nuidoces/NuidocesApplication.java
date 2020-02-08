2e98321baf011f8ca52dd0b3c3678e829c341f99
Ricardo Sérgio <rscarvalho@hotmail.com>
2020-02-08 18:55:40 -0300

    Projeto criado

---
+++ src/main/java/com/ricardo/nuidoces/NuidocesApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.nuidoces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuidocesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuidocesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/nuidoces/NuidocesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.nuidoces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuidocesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
