ea5ef7c33d3ce526245dadff717b53d2f4835bb7
Ricardo Sérgio <rscarvalho@hotmail.com>
2020-02-08 21:53:52 -0300

    Criando operação de instanciação

--- src/main/java/com/ricardo/nuidoces/NuidocesApplication.java
+++ src/main/java/com/ricardo/nuidoces/NuidocesApplication.java
@@ -1,13 +1,35 @@
 package com.ricardo.nuidoces;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ricardo.nuidoces.domain.Categoria;
+import com.ricardo.nuidoces.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class NuidocesApplication {
+public class NuidocesApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NuidocesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Doces Simples");
+		Categoria cat2 = new Categoria(null, "Doces Goumert");
+		Categoria cat3 = new Categoria(null, "Torta de morango");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		
 	}
 
 }
